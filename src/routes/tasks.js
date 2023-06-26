21eaecf68374f6fd59582af858b3634dcd3e4a9a
Leonardo dos Santos <leodoima@hotmail.com>
2023-06-26 09:06:11 -0300

    Aplicando uso de middlewares no controller das tasks

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,21 +1,73 @@
 const express = require('express')
-const { route } = require('./users')
+const { v4: uuidv4 } = require('uuid');
 const router = express.Router()
+const tasks = []
+
+
+function validIfExistsTask(req, res, next){
+    const { id } = req.params;
+    const task = tasks.find(t => t.id === id);
+
+    if (!task) {
+        return res.status(400).json({ error: "Task not found" })
+    }
+
+    next()
+};
+
+
+router.use((req, res, next) => {
+    const { account } = req.headers;
+
+    console.log(`[${req.method}] - ${req.path} ${Date.now()}`)
+
+    if (!account) {
+        return res.status(401).json({ error: "User unauthorized" })
+    }
+
+    next()
+})
 
 
 router.get('/', (req, res) => {
-    res.send('Got a GET request!')
+    return res.send(tasks)
+})
+
+router.get('/:id', validIfExistsTask, (req, res) => {
+
+    const task = validIfExistsTask.task
+
+    return res.send(task)
 })
 
 router.post('/', (req, res) => {
-    res.send('Got a POST request')
+    const { description, appointment } = req.body;
+    const { account } = req.headers;
+
+    const task = {
+        id: uuidv4(),
+        account: account,
+        description: description,
+        appointment: appointment,
+        created_at: new Date()
+    }
+
+    tasks.push(task)
+
+    return res.status(201).json(task)
 })
 
-router.put((req, res) => {
-    res.send('Got a PUT request at /user')
+router.put('/:id', validIfExistsTask, (req, res) => {
+    const { id } = req.params;
+    const { account } = req.headers;
+    const { description, appointment} = req.body;
+
+    res.send()
+
+
 })
 
-router.delete((req, res) => {
+router.delete('/:id', validIfExistsTask, (req, res) => {
     res.send('Got a DELETE request at /user')
 })
 
