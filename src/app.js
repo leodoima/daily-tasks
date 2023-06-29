bdf90586633c1511c8e98e3c51565c5509653111
Leonardo dos Santos <leodoima@hotmail.com>
2023-06-28 21:33:01 -0300

    Reestruturando o projeto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
+
+const tasksRoutes = require('./routes/tasks.routes')
+
 const app = express()
 const port = 3000
 
-const tasks = require('./routes/tasks')
 
 app.use(express.json())
-app.use('/tasks', tasks)
+app.use('/tasks', tasksRoutes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
